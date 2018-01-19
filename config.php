12f27937edd6b2ae0b5245a97dc74ee85a2335c7
floppy0 <31189947+floppy0@users.noreply.github.com>
2018-01-19 13:12:50 +0100

    config

--- config.php
+++ config.php
@@ -8,10 +8,11 @@ define('PATH_METADATA', PATH_ROOT.'metadata'.DS);
 define('PATH_ITEMS', PATH_METADATA.'items'.DS);
 define('PATH_AUTHORS', PATH_METADATA.'authors'.DS);
 define('CHARSET', 'UTF-8');
-define('DOMAIN', 'http://localhost:8000');
-#define('DOMAIN', 'https://plugins.bludit.com');
-define('CDN', 'http://localhost:8000/metadata/items/');
-#define('CDN', 'https://rawgit.com/bludit/plugins-repository/master/');
+#define('DOMAIN', 'http://localhost:8000');
+define('DOMAIN', 'https://plugins.bludit.com');
+#define('CDN', 'http://localhost:8000/metadata/items/');
+define('CDN', 'https://rawgit.com/bludit/plugins-repository/master/');
+
 define('ITEM_TYPE', 'plugin');
 #define('ITEM_TYPE', 'theme');
 

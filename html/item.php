bb75dc0e7be5b8f8b8b41784f4fe1ee2ca3dcbbd
Diego Najar <dignajar@gmail.com>
2018-12-27 12:52:19 +0100

    Include link to metadata

--- html/item.php
+++ html/item.php
@@ -6,8 +6,8 @@
 
 		<h1 style="font-size: 1.6em;" class="my-4"><?php echo $_item['name'] ?></h1>
 
-		<?php if (!$data['translated']): ?>
-			<div class="alert alert-warning"><?php echo l("this-plugin-has-not-yet-been-translated-into") ?></div>
+		<?php if (!$_item['translated']): ?>
+			<div class="alert alert-warning"><a target="_blank" href="https://github.com/bludit/plugins-repository/tree/master/items/<?php echo $_item['key'] ?>"><?php echo l("this-plugin-has-not-yet-been-translated-into") ?></a></div>
 		<?php endif; ?>
 
 		<div class="my-4">

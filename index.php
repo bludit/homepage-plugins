ef68cbf2a8e5f1d48cb518c376b6caaa77ebe1b5
Diego Najar <dignajar@gmail.com>
2019-01-20 00:02:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -24,8 +24,8 @@
 	<?php include(PATH_HTML.'footer.php'); ?>
 
 	<!-- Javascript stuff -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js"></script>
 
 	<script>
 	$(document).ready(function() {

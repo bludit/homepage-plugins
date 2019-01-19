2d98f138a8e933887523ca8160207d112123a8c7
Diego Najar <dignajar@gmail.com>
2019-01-19 23:44:10 +0100

    Update head.php

--- html/head.php
+++ html/head.php
@@ -7,8 +7,8 @@
 
 	<?php
 		if ($_item!==false) {
-			echo '<title>'.htmlentities($_item['name'], ENT_QUOTES).' - '.l('head title', false).'</title>'.PHP_EOL;
-			echo '<meta name="description" content="'.htmlentities($_item['description'], ENT_QUOTES).'">'.PHP_EOL;
+			echo '<title>'.htmlentities(strip_tags($_item['name']), ENT_QUOTES).' - '.l('head title', false).'</title>'.PHP_EOL;
+			echo '<meta name="description" content="'.htmlentities(strip_tags($_item['description']), ENT_QUOTES).'">'.PHP_EOL;
 		} else {
 			echo '<title>'.l('head title', false).'</title>'.PHP_EOL;
 			echo '<meta name="description" content="'.l('head description', false).'">'.PHP_EOL;
@@ -23,8 +23,8 @@
 	<meta name="twitter:site" content="@bludit" />
 	<?php
 		if ($_item!==false) {
-			echo '<meta name="twitter:title" content="'.htmlentities($_item['name'], ENT_QUOTES).' - '.l('head title', false).'" />'.PHP_EOL;
-			echo '<meta name="twitter:description" content="'.htmlentities($_item['description'], ENT_QUOTES).'" />'.PHP_EOL;
+			echo '<meta name="twitter:title" content="'.htmlentities(strip_tags($_item['name']), ENT_QUOTES).' - '.l('head title', false).'" />'.PHP_EOL;
+			echo '<meta name="twitter:description" content="'.htmlentities(strip_tags($_item['description']), ENT_QUOTES).'" />'.PHP_EOL;
 			echo '<meta name="twitter:image" content="'.$_item['screenshoot_twitter_url'].'" />'.PHP_EOL;
 		} else {
 			echo '<meta name="twitter:title" content="'.l('head title', false).'" />'.PHP_EOL;
@@ -42,8 +42,8 @@
 	<?php
 		if ($_item!==false) {
 			echo '<meta property="og:url" content="'.$_item['permalink'].'" />'.PHP_EOL;
-			echo '<meta property="og:title" content="'.htmlentities($_item['name'], ENT_QUOTES).' - '.l('head title', false).'" />'.PHP_EOL;
-			echo '<meta property="og:description" content="'.htmlentities($_item['description'], ENT_QUOTES).'" />'.PHP_EOL;
+			echo '<meta property="og:title" content="'.htmlentities(strip_tags($_item['name']), ENT_QUOTES).' - '.l('head title', false).'" />'.PHP_EOL;
+			echo '<meta property="og:description" content="'.htmlentities(strip_tags($_item['description']), ENT_QUOTES).'" />'.PHP_EOL;
 			echo '<meta property="og:image" content="'.$_item['screenshoot_facebook_url'].'" />'.PHP_EOL;
 		} else {
 			echo '<meta property="og:url" content="'.$_topbar['website'].'" />'.PHP_EOL;

edfe480f9a4a9777778351875eeeb2979ba815b8
Diego Najar <dignajar@gmail.com>
2019-01-19 15:16:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -46,5 +46,25 @@
 	});
 	</script>
 
+	<!--
+		Yandex.Metrika counter
+		Masking IP addresses enabled for GDPR
+	-->
+	<script type="text/javascript" >
+	   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+	   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+	   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+	   ym(51979046, "init", {
+		id:51979046,
+		clickmap:true,
+		trackLinks:true,
+		webvisor:true,
+		accurateTrackBounce:true
+	   });
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/51979046" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
+	
 </body>
 </html>

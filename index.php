0a2c492478b0b1e7a3087ab0c60beb753e7272ae
Diego Najar <dignajar@gmail.com>
2019-05-07 23:02:37 +0200

    remove type javascript

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ ini_set('display_startup_errors', TRUE);
 		Yandex.Metrika counter
 		Masking IP addresses enabled for GDPR
 	-->
-	<script type="text/javascript" >
+	<script>
 	   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
 	   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
 	   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
